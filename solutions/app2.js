f08e08a7abbbbcaf7918377c4566d9d2e8dcab20
BKMarcsi <bkolonuszmaria@gmail.com>
2021-11-21 22:28:08 +0100

    js4_001

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,16 @@
+const sorterPromise = (initArray = []) => {
+    return new Promise((resolve, reject) => {
+        if (initArray.every(element => typeof(element) === 'string')){
+            resolve(initArray.sort());
+        }
+        else{
+            reject('Error: Not all elements are string type!');
+        }
+    });
+}
 
 
 
 
 
-
-// export default sorterPromise;
+export default sorterPromise;
